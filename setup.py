f1e2e7c3d7114fab11e45a96273ff079ece79f34
Johan Walles <walles@spotify.com>
2015-05-28 10:53:00 +0200

    Add an initial setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+install_requires = (
+    'pyaml',
+    'crtauth',
+    'requests==2.6.0',
+)
+
+tests_require = install_requires
+
+
+setup(
+    name='drserv',
+    version='0.1.0',
+    url='https://github.com/spotify/drserv',
+    author='Client Build Squad @ Spotify',
+    author_email='client-build@spotify.com',
+    description=('RESTful service for publishing packages to debian '
+                 'repositories'),
+    license='Apache 2.0',
+    packages=['drserv'],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Build Tools',
+    ],
+)
